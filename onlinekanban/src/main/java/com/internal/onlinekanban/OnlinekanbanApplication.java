482a0f52991144ee6a72c035d552812f3f3115d6
Rauf Aliyev <mr.aliyevrauf@gmail.com>
2019-12-29 14:21:34 +0400

    Initial commit

---
+++ onlinekanban/src/main/java/com/internal/onlinekanban/OnlinekanbanApplication.java
@@ -0,0 +1,13 @@
+package com.internal.onlinekanban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinekanbanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinekanbanApplication.class, args);
+	}
+
+}
---
+++ onlinekanban/src/test/java/com/internal/onlinekanban/OnlinekanbanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internal.onlinekanban;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinekanbanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
